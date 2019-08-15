0e92fdbf434de5b60f2f0d04b8ad1dfe12a3faba
Phoebe Law <50650237+shiningjustice@users.noreply.github.com>
2019-08-15 16:11:50 -0700

    First Commit - In progress
    HTML/CSS provided by Thinkful and is complete.
    JS add function complete. Check function runs with click but doesn't execute code as needed. Delete function deletes properly, except for whatever was added after the code started running.

---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict';
+// Hint: you may find it helpful to read up on and use the following jQuery methods: .submit(), preventDefault(), toggleClass(), and closest().
+
+// enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
+function addNewItem () {
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    const newItem = $('#shopping-list-entry').val();
+   $('.shopping-list').append(`
+      <li>
+        <span class="shopping-item">${newItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
+  });
+}
+
+// check and uncheck items (strikethrough) on the list by clicking the "Check" button
+function checkUncheckItem () {
+  console.log('function checkUncheckItem called successfully');
+  $('.shopping-item-toggle').on('click', function(event) {
+    // we don't need prevent default bc button doesn't have default behavior
+    //this "check uncheck" portion doesn't run; method must be wrong per console.log not running
+    console.log('Check button clicked successfully');
+    $(event.target).closest('.shopping-item').toggleClass('.shopping-item__checked');
+  });
+}
+
+// permanently remove items from the list
+// this ALMOST works! problem is that it can't delete items that were added after the page was loaded :/
+function deleteItem () {
+  console.log('function deleteItem called successfully');
+  $('.shopping-item-delete').on('click', function(event) {
+    console.log('Delete button clicked successfully');
+    $(event.target).closest("li").remove();
+  });
+}
+
+$(addNewItem);
+$(checkUncheckItem);
+$(deleteItem);
